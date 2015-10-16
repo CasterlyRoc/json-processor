fd34dd861e4e890d58070a3f350f9b394c5b26ed
Casterly_Roc <zknopp@terpmail.umd.edu>
2015-10-16 11:12:49 -0400

    t

--- JSONProcessing.java
+++ JSONProcessing.java
@@ -12,7 +12,7 @@ public class JSONProcessing
 		try {
 			JSONParser jsonParser = new JSONParser();
 			JSONObject jsonObject = (JSONObject) jsonParser.parse(json);
-			JSONArray test = jsonObject.get("statusupdate");
+			JSONArray test = jsonObject.values();
 			System.out.print(test.toString());
 		} catch (ParseException e){
 			e.printStackTrace();
@@ -23,9 +23,9 @@ public class JSONProcessing
 	public static void main(String[] argv) {
 		Scanner in_scanner = new Scanner(System.in);
 
-		while(in_scanner.hasNext()) {
+
 			String json = in_scanner.nextLine();
 			processJSON(json);
-		}
+
 	}
 }
